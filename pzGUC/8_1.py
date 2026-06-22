45ab1ae8c9453870a6e568c17e172d9c1f4473a4
Выходи из аккаунта когда закончил работу <weweabc68@gmail.com>
2026-06-22 13:08:14 +0300

    123
    123

---
+++ pzGUC/8_1.py
@@ -0,0 +1,6 @@
+#Вариант 5
+#1. Выведите возраст из data = {'person': {'name': 'Alice', 'age': 30}}.
+
+data = {'person': {'name': 'Alice', 'age': 30}}
+age = data['person']['age']
+print(f"Возраст: {age}")
