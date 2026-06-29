0b46cde357267a81bdf9ad36ee5d1ee7371532a0
Владимир Гуц <weweabc68@gmail.com>
2026-06-29 13:49:23 +0300

    Delete pzGUC directory

--- pzGUC/8_1.py
+++
@@ -1,6 +0,0 @@
-#Вариант 5
-#1. Выведите возраст из data = {'person': {'name': 'Alice', 'age': 30}}.
-
-data = {'person': {'name': 'Alice', 'age': 30}}
-age = data['person']['age']
-print(f"Возраст: {age}")
